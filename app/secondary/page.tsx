4ccd1881701debee7809cab7834faaf32773cbe9
pranto54rudro-sketch <pranto54rudro@gmail.com>
2026-09-14 01:11:29 +0600

    feat: wire settlement control into secondary business

--- app/secondary/page.tsx
+++ app/secondary/page.tsx
@@ -1,3 +1,4 @@
 'use client';
 import SecondaryBusinessV2 from '@/components/SecondaryBusinessV2';
-export default function SecondaryBusiness(){return <SecondaryBusinessV2/>;}
+import SettlementControl from '@/components/SettlementControl';
+export default function SecondaryBusiness(){return <><SecondaryBusinessV2/><SettlementControl/></>;}
