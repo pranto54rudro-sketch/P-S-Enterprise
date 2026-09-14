726f8163fc3a27bf6856bda821fa34d921499c6d
pranto54rudro-sketch <pranto54rudro@gmail.com>
2026-09-14 11:52:16 +0600

    feat: mount rebuilt business control center

--- app/secondary/page.tsx
+++ app/secondary/page.tsx
@@ -1,5 +1,5 @@
 'use client';
 import PremiumSecondaryBusiness from '@/components/PremiumSecondaryBusiness';
-import BusinessControlOverlay from '@/components/BusinessControlOverlay';
+import BusinessControlCenterV2 from '@/components/BusinessControlCenterV2';
 import ExecutiveSecondaryTheme from '@/components/ExecutiveSecondaryTheme';
-export default function SecondaryBusiness(){return <><PremiumSecondaryBusiness/><BusinessControlOverlay/><ExecutiveSecondaryTheme/></>;}
+export default function SecondaryBusiness(){return <><PremiumSecondaryBusiness/><BusinessControlCenterV2/><ExecutiveSecondaryTheme/></>;}
